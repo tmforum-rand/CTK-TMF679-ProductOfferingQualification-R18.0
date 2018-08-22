bf7b139a55e44ef02fa9f3519a500d083b779795
Henrique Rodrigues <henrique@promotops.com.br>
2018-08-22 14:01:16 +0100

    Newman is auto installed now

--- Windows-RUNCTK-TMF679-ProductOfferingQualification.ps1
+++ Windows-RUNCTK-TMF679-ProductOfferingQualification.ps1
@@ -1,6 +1,7 @@
 Write-Host "This will run a TMForum API CTK"
 Write-Host "In order to be able to run it, you need to have"
-Write-Host "NodeJS, NMP and Newman installed."
+Write-Host "NodeJS and NPM installed."
+Write-Host "Installing npm dependencies..."
 Write-Host
 
 
